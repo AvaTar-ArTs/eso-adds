555fc7eb3e7d977b1427cf1315dc06381354cee7
AvaTar-ArTs <me@avatararts.org>
2026-09-04 18:46:42 -0400

    Add EPB constants

---
+++ ESOProfileBridge/core/Constants.lua
@@ -0,0 +1,6 @@
+ESOProfileBridge = ESOProfileBridge or {}
+local EPB = ESOProfileBridge
+
+EPB.SV_VERSION = 1
+EPB.MAX_EXPORT_CHUNK = 320 -- leaves room under ESO chat's practical message limit
+EPB.PROFILE_SCHEMA_VERSION = 1
